57a9765409aa76f820cf2ff3ade5a7507ad0c61a
Jim Shaw <jimxshaw@gmail.com>
2016-11-22 20:11:03 -0600

    Tested observers some more.

--- RxJS/main.js
+++ RxJS/main.js
@@ -2,27 +2,17 @@
 var rxjs_1 = require("rxjs");
 var numbers = [1, 2, 3, 5, 7, 11];
 var source = rxjs_1.Observable.create(function (observer) {
-    for (var _i = 0, numbers_1 = numbers; _i < numbers_1.length; _i++) {
-        var n = numbers_1[_i];
-        observer.next(n);
-    }
-    observer.complete();
+    var index = 0;
+    var produceValue = function () {
+        observer.next(numbers[index++]);
+        if (index < numbers.length) {
+            setTimeout(produceValue, 2000);
+        }
+        else {
+            observer.complete();
+        }
+    };
+    produceValue();
 });
-// Observers can be created using shorthand with lambdas or
-// below with an actual class.
 source.subscribe(function (value) { return console.log("value: " + value); }, function (error) { return console.log("error: " + error); }, function () { return console.log("complete"); });
-// class MyObserver implements Observer<number> {
-//
-//     next(value) {
-//         console.log(`value: ${value}`);
-//     }
-//
-//     error(e) {
-//         console.log(`error: ${e}`);
-//     }
-//
-//     complete() {
-//         console.log("complete");
-//     }
-// }
 //# sourceMappingURL=main.js.map
--- RxJS/main.ts
+++ RxJS/main.ts
@@ -1,38 +1,30 @@
-import { Observable, Observer } from "rxjs";
+import { Observable } from "rxjs";
 
 let numbers = [1, 2, 3, 5, 7, 11];
 
 let source = Observable.create(observer => {
 
-    for (let n of numbers) {
-        observer.next(n);
-    }
+    let index = 0;
 
-    observer.complete();
+    let produceValue = () => {
+        observer.next(numbers[index++]);
+
+        if (index < numbers.length) {
+            setTimeout(produceValue, 2000);
+        }
+        else {
+            observer.complete();
+        }
+    };
+
+    produceValue();
 
 });
 
-// Observers can be created using shorthand with lambdas or
-// below with an actual class.
 source.subscribe(
     value => console.log(`value: ${value}`),
     error => console.log(`error: ${error}`),
     () => console.log("complete")
 );
-
-// class MyObserver implements Observer<number> {
-//
-//     next(value) {
-//         console.log(`value: ${value}`);
-//     }
-//
-//     error(e) {
-//         console.log(`error: ${e}`);
-//     }
-//
-//     complete() {
-//         console.log("complete");
-//     }
-// }
 
 

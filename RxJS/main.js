d9956981fcda41f44f7d9be2ad177e46184284d3
Jim Shaw <jimxshaw@gmail.com>
2016-11-22 19:59:27 -0600

    Created an observer class that uses an observable.

--- RxJS/main.js
+++ RxJS/main.js
@@ -1,2 +1,20 @@
-alert("Hello World!");
+"use strict";
+var rxjs_1 = require("rxjs");
+var numbers = [1, 2, 3, 5, 7, 11];
+var source = rxjs_1.Observable.from(numbers);
+var MyObserver = (function () {
+    function MyObserver() {
+    }
+    MyObserver.prototype.next = function (value) {
+        console.log("value: " + value);
+    };
+    MyObserver.prototype.error = function (e) {
+        console.log("error: " + e);
+    };
+    MyObserver.prototype.complete = function () {
+        console.log("complete");
+    };
+    return MyObserver;
+}());
+source.subscribe(new MyObserver());
 //# sourceMappingURL=main.js.map
--- RxJS/main.ts
+++ RxJS/main.ts
@@ -1,1 +1,22 @@
-alert("Hello World!");
+import { Observable, Observer } from "rxjs";
+
+let numbers = [1, 2, 3, 5, 7, 11];
+
+let source = Observable.from(numbers);
+
+class MyObserver implements Observer<number> {
+
+    next(value) {
+        console.log(`value: ${value}`);
+    }
+
+    error(e) {
+        console.log(`error: ${e}`);
+    }
+
+    complete() {
+        console.log("complete");
+    }
+}
+
+source.subscribe(new MyObserver());

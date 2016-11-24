cc4cb712223382e037f531ff402fee3772c8f9eb
Jim Shaw <jimxshaw@gmail.com>
2016-11-24 14:31:59 -0600

    Tested flatMap operator.

--- RxJS/main.js
+++ RxJS/main.js
@@ -4,17 +4,24 @@ var output = document.getElementById("output");
 var button = document.getElementById("button");
 var click = rxjs_1.Observable.fromEvent(button, "click");
 function load(url) {
-    var xhr = new XMLHttpRequest();
-    xhr.addEventListener("load", function () {
-        var movies = JSON.parse(xhr.responseText);
-        movies.forEach(function (m) {
-            var div = document.createElement("div");
-            div.innerText = m.title;
-            output.appendChild(div);
+    return rxjs_1.Observable.create(function (observer) {
+        var xhr = new XMLHttpRequest();
+        xhr.addEventListener("load", function () {
+            var data = JSON.parse(xhr.responseText);
+            observer.next(data);
+            observer.complete();
         });
+        xhr.open("GET", url);
+        xhr.send();
     });
-    xhr.open("GET", url);
-    xhr.send();
 }
-click.subscribe(function (e) { return load("movies.json"); }, function (error) { return console.log("error: " + error); }, function () { return console.log("complete"); });
+function renderMovies(movies) {
+    movies.forEach(function (m) {
+        var div = document.createElement("div");
+        div.innerText = m.title;
+        output.appendChild(div);
+    });
+}
+click.flatMap(function (e) { return load("movies.json"); })
+    .subscribe(renderMovies, function (error) { return console.log("error: " + error); }, function () { return console.log("complete"); });
 //# sourceMappingURL=main.js.map
